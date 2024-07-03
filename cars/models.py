31eb09e8cfd94376f3415ee87dee0c6766e6c453
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-03 14:25:15 -0300

    Criei o modelo da tabela de carros

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Criando a tabela do banco de dados dos carros
+class Car(models.Model):
+    id = models.AutoField(primary_key=True) # AutoField = Autoincremeto
+    model = models.CharField(max_length=200) # Charfield = String
+    brand = models.CharField(max_length=200)
+    factory_year = models.IntegerField(blank=True, null=True) # IntegerField = Inteiro
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True) # FloatField = Float
