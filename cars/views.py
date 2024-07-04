4e310ffd4733d77b4df60033f73d0d1493cc8929
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-04 15:54:28 -0300

    Atualizado template

--- cars/views.py
+++ cars/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def cars_view(request):
-    return render(request, template_name='cars.html')
+    return render(
+                request,
+                template_name='cars.html', 
+                context={'cars': {'model':'Civic 1.7'}}
+                )
