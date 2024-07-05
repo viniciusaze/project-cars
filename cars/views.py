9251249d9b50cf2935b738513a16ca1bd102b2d4
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-05 14:26:39 -0300

    Att cars_view

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,11 @@ from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    cars = Car.objects.filter(model__contains='R') # Busca os dados no bd
+    search = request.GET.get('search')
+
+    request.GET.get('search')
+
+    cars = Car.objects.filter(model__contains=search) # Busca os dados no bd
 
     return render(
                 request,
