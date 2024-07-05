66f759ec1d4ee75b78699b396f6d0c93215eb7a8
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-05 15:14:39 -0300

    Criado template base cars

--- cars/views.py
+++ cars/views.py
@@ -3,11 +3,13 @@ from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    cars = Car.objects.all() # Busca todos os dados no bd
+    cars = Car.objects.all().order_by('model') 
+    # Busca todos os dados no bd
     search = request.GET.get('search')
 
     if search:
-        cars = Car.objects.filter(model__contains=search) # Busca os dados no bd com filtro
+        cars = Car.objects.filter(model__icontains=search)
+        # Busca os dados no bd com filtro
 
     return render(
                 request,
