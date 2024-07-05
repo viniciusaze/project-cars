40b382fde8cedf94b09ea78378ddb1be5519515e
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-05 15:25:06 -0300

    Template base

--- app/settings.py
+++ app/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['app/templates'], # Adicionada a pasta templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
