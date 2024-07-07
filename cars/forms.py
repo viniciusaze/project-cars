19ef8781a1a5836754168d7d10acc6529dea8156
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-07 14:27:30 -0300

    Criado cadastro de carros com forms

--- cars/forms.py
+++ cars/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Brand, Car
 
 
 class CarForm(forms.Form):
@@ -12,3 +12,15 @@ class CarForm(forms.Form):
     photo = forms.ImageField()
 
 
+    def save(self):
+        car = Car(
+            model = self.cleaned_data['model'],
+            brand = self.cleaned_data['brand'],
+            factory_year = self.cleaned_data['factory_year'],
+            model_year = self.cleaned_data['model_year'],
+            plate = self.cleaned_data['plate'],
+            value = self.cleaned_data['value'],
+            photo = self.cleaned_data['photo']
+        )
+        car.save() # Este save é o que salva no BD, é o save do objeto Car
+        return car
