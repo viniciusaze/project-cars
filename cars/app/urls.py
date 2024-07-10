61cafe27a2f5d0c111fb90f7443e482ef9f90a03
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-10 13:57:05 -0300

    Atualizando as CBVs

--- cars/app/urls.py
+++ cars/app/urls.py
@@ -18,13 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import NewCarView, CarsView
+from cars.views import NewCarView, CarsListView
 from accounts.views import register_view, login_view, logout_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', CarsView.as_view(), name='cars_list'),
+    path('cars/', CarsListView.as_view(), name='cars_list'),
     path('new_car/', NewCarView.as_view(), name='new_car'),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
--- cars/cars/views.py
+++ cars/cars/views.py
@@ -1,7 +1,10 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView
 
 
 # Create your views here.
@@ -21,6 +24,20 @@ class CarsView(View):
                 'cars.html', 
                 {'cars': cars}
                 )
+    
+
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    # Para executar o filtro
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
 
 
 class NewCarView(View):
