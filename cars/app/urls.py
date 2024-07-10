c05113f5e7eb50aa9aedbd504b827127055cb4be
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-10 13:21:58 -0300

    Reescrevendo views

--- cars/app/urls.py
+++ cars/app/urls.py
@@ -18,14 +18,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import new_car_view, CarsView
+from cars.views import NewCarView, CarsView
 from accounts.views import register_view, login_view, logout_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', CarsView.as_view(), name='cars_list'),
-    path('new_car/', new_car_view, name='new_car'),
+    path('new_car/', NewCarView.as_view(), name='new_car'),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout')
--- cars/cars/views.py
+++ cars/cars/views.py
@@ -38,3 +38,23 @@ def new_car_view(request):
                 )
 
 
+class NewCarView(View):
+    
+    def get(self, request):
+        new_car_form = CarModelForm
+        return render(
+                request,
+                'new_car.html',
+                {'new_car_form': new_car_form }
+                )
+    
+    def post(self, request):
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+        return render(
+                request,
+                'new_car.html',
+                {'new_car_form': new_car_form }
+                )
