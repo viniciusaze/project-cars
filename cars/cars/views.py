472a7ec833ccec0e73d72c0cf0d03980824572ea
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-11 14:17:34 -0300

    Update

--- cars/cars/views.py
+++ cars/cars/views.py
@@ -1,5 +1,6 @@
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
@@ -34,7 +35,9 @@ class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
     template_name = 'car_update.html'
-    success_url = '/cars/'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
 
 class CarDeleteView(DeleteView):
