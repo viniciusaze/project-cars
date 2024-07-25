52a7eaf6e2154b797e45ee171c6eea45f2c50ebf
Vinícius Azevedo <ar-vinicius@hotmail.com>
2024-07-25 16:29:11 -0300

    Adicionando inventario a pag de admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from cars.models import Car
-from cars.models import Brand
+from cars.models import Car, Brand, CarInventory
+
 
 # Adicionando o modelo Car na sessão admin
 class CarAdmin(admin.ModelAdmin):
@@ -12,6 +12,12 @@ class BrandAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
 
+
+class CarInvetoryAdmin(admin.ModelAdmin):
+    list_display = ('cars_count', 'cars_value', 'created_at')
+    search_fields = ('created_at',)
+
 # Registrando no admin
 admin.site.register(Car, CarAdmin) # Parametros = Model e Admin
 admin.site.register(Brand, BrandAdmin)
+admin.site.register(CarInventory, CarInvetoryAdmin)
